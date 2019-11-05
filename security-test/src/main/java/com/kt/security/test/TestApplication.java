76bcecc061bff2fa36381fc4c3a536798bf5616d
KeenTang <keen1991@126.com>
2019-11-05 23:54:20 +0800

    init

---
+++ security-test/src/main/java/com/kt/security/test/TestApplication.java
@@ -0,0 +1,19 @@
+package com.kt.security.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Desc:
+ *
+ * @author: keen
+ * Date: 2019-11-05
+ * Time: 23:41
+ */
+@SpringBootApplication
+public class TestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+    }
+}
